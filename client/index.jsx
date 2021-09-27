8bb494d303ad9ea1323ac9ff2e498aa397454940
eric-olaya <ericolaya@gmail.com>
2021-09-27 19:43:04 -0400

    boilerplate code set up

---
+++ client/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './containers/App';
+import './styles/stylesheet.scss';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
---
+++ server/server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+
+// parse incoming data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+// send bundle
+app.use('/build', express.static(path.join(__dirname, '../build/')));
+app.get('/', (req, res) => {
+  return res.status(200).sendFile(path.resolve(__dirname, '../index.html'));
+});
+
+
+//catch all route handler, handles request to an unknown route
+app.use((req, res) =>
+  res.status(404).send('The page you are looking for not exist.')
+);
+
+//gloabal error handler
+app.use((err, req, res, next) => {
+  console.log(err);
+  return res
+    .status(500)
+    .send('Unknown error in middleware. See server logs for more information.');
+});
+
+app.listen(3000, () => console.log('Listening on port 3000'));
