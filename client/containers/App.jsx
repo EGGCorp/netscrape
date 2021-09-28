cc154cf140ec6956a196f954f1b187a1748a5e0c
Monchichi <monchichi@Monchichis-Air.lan>
2021-09-28 11:49:20 -0400

    App displays on screen

---
+++ client/containers/App.jsx
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+
+    }
+  }
+  render() {
+
+    return (
+      <div>
+        I am an app
+      </div>
+    )
+  }
+}
+
+export default App;
