ad7e978b6fa75a2a7d1915be2e8d727a9fac1fe6
George <iSaySureWhyNot@gmail.com>
2021-09-28 13:32:00 -0400

    added boiler plate db controllers

---
+++ server/dbController.js
@@ -0,0 +1,13 @@
+const db = require('./models');
+
+const dbController = {};
+
+dbController.addMovies = (req, res, next) => {
+    db
+        .query()
+}
+
+dbController.getMovies = (req, res, next) => {
+    db
+        .query()
+}
