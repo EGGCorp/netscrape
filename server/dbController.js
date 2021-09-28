30f88fdff2d2e9cfc0871d525cb9a1e41018e911
eric-olaya <ericolaya@gmail.com>
2021-09-28 17:54:35 -0400

    added axios fetch functionality and git ignore

---
+++ server/dbController.js
@@ -0,0 +1,15 @@
+const db = require('./models');
+
+const dbController = {};
+
+dbController.addMovies = (req, res, next) => {
+    db
+        .query()
+}
+
+dbController.getMovies = (req, res, next) => {
+    db
+        .query()
+}
+
+module.exports = dbController;
