5fab8fa9eb09b09575d163d2175ab5b2e8c51b86
eric-olaya <ericolaya@gmail.com>
2021-09-29 11:24:48 -0400

    styled and organized app display

--- client/containers/App.jsx
+++ client/containers/App.jsx
@@ -2,14 +2,14 @@ import React, { Component } from 'react';
 import SearchBar from '../components/SearchBar.jsx';
 import SearchResultsBox from '../components/SearchResultsBox.jsx';
 import getMovies from '../../api/movieFetch.js';
-
+import MovieCard from '../components/MovieCard.jsx';
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
       searchedTitle: 'string',
-      searchResults: {},
+      searchResults: [],
     }
     
     this.onKeyUp = this.onKeyUp.bind(this)
@@ -21,23 +21,34 @@ class App extends Component {
       console.log(e.target.value)
       const value = e.target.value;
       const movieList = await getMovies(value);
+      //const movies = [];
       console.log('movie list', movieList);
       this.setState({ searchedTitle: e.target.value }, () => {
         console.log('this is the setState', this.state.searchedTitle)
       
       })
-      
     }
   } 
 
-  render() {
 
+  render() {
+    console.log('state in render',this.state)
     return (
-      <div>
-        <SearchBar onKeyUp={this.onKeyUp}/>
-        <SearchResultsBox value={this.searchResults}/>
+      <div id='app'>
+        <h1>Netscrape</h1>
+        <div className='container'>
+          <div className='mainContent'>
+            <SearchBar id='searchBar' onKeyUp={this.onKeyUp}/>
+            <SearchResultsBox id='searchResultsBox'value={this.searchResults}/>
+          </div>
+        <div className='rightSideBar'>
+          <h2>Top Rated Movies</h2>
+
+        </div>
+        <div className='footer'>
+        </div>
+        </div>
       </div>
-      
     )
   }
 }
